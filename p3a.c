bb89406b5b0c5ab8d5158ae43bd3e58484118e4f
Aalok Pandit <51901277+alowkii@users.noreply.github.com>
2024-08-13 02:28:38 +0530

    Update p3a.c

--- p3a.c
+++ p3a.c
@@ -1,49 +1,25 @@
 #include<stdio.h>
-
 #include<ctype.h>
-
 #include<string.h>
-
 #define size 50
 
-
-
 int main(){
-
 	char text[size], key[size], ctext[size];
 
-	
-
 	printf("Enter the text:");
-
 	fgets(text, size, stdin);
-
 	
-
 	printf("Enter the key:");
-
 	fgets(key, size, stdin);
 
-	
-
 	int i = 0;
-
 	while(isalpha(text[i]) && isalpha(key[i])){
-
 		ctext[i] = tolower(text[i])+tolower(key[i])-'a'*2;
-
 		ctext[i] %= 26;
-
 		ctext[i] += 'a';
-
 		i++;
-
 	}
 
-	
-
 	ctext[i] = '\0';
-
 	printf("%s",ctext);
-
 }
